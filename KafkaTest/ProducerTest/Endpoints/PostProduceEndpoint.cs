d7ebac825607b47285d341d25ad8b6b40c057dec
Hardik Acharya <hbacharya@gmail.com>
2026-09-01 21:17:35 +0530

    Kafka Increase message size (producer change)

--- KafkaTest/ProducerTest/Endpoints/PostProduceEndpoint.cs
+++ KafkaTest/ProducerTest/Endpoints/PostProduceEndpoint.cs
@@ -32,27 +32,48 @@ namespace ProducerTest.Endpoints
 
         public override async Task HandleAsync(PostProduceRequest req, CancellationToken ct)
         {
+            // await Send.OkAsync(new ProducerResponse
+            // {
+            //     Offset = -1 // Placeholder, actual offset will be set after successful production
+            // }, ct);
+            // return;
+            
             var bootstrapServers = req.Servers; //_configuration["Kafka:BootstrapServers"]!;
             var topic = req.Topic; //_configuration["Kafka:Topic"]!;
 
             var producerConfig = new ProducerConfig
             {
-                BootstrapServers = bootstrapServers
+                BootstrapServers = bootstrapServers,
+                MessageMaxBytes = 20971520
             };
 
             using var producer = new ProducerBuilder<string, string>(producerConfig).Build();
 
-            var deliveryResult = await producer.ProduceAsync(
-                topic,
-                new Message<string, string> {Key = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"), Value = req.Message },
-                ct);
+            var filePath = @"C:\Hardik\Learn\devops\DevOpsTry\KafkaTest\ProducerTest\bin\Debug\net10.0\NSwag.AspNetCore.dll";
+            var bytes = File.ReadAllBytes(filePath);
+            var fileText = Convert.ToBase64String(bytes);
+            var reqMessage = fileText; //req.Message;
 
-            var response = new ProducerResponse
+            try
             {
-                Offset = deliveryResult.Offset.Value
-            };
+                var deliveryResult = await producer.ProduceAsync(
+                    topic,
+                    new Message<string, string>
+                        { Key = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss fff"), Value = reqMessage },
+                    ct);
 
-            await Send.OkAsync(response, ct);
+                var response = new ProducerResponse
+                {
+                    Offset = deliveryResult.Offset.Value
+                };
+
+                await Send.OkAsync(response, ct);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                throw;
+            }
         }
     }
 }
